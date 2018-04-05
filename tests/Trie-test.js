452f39665cada00986e6435cfca6b1fad6e3314a
Helen Dechat <hdechat@gmail.com>
2018-04-04 23:30:58 -0600

    Redo tests and code, insert method, and pseudocode suggest method

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -1,54 +1,59 @@
 import { expect } from 'chai';
-
 import Trie from '../lib/Trie.js';
 
 describe('Trie', () => {
   let trie;
 
-  beforeEach(() => {
-    trie = new Trie();
+  beforeEach(() => trie = new Trie());
+
+  it('should have a root node defaulted to null', () => {
+    expect(trie.root).to.equal(null);
   });
 
-  it('should have a root node defaulted to a period', () => {
-    expect(trie.root.data).to.equal('.');
-  });
-
-  describe('insert', () => {
+  describe('INSERT', () => {
     it('should be able to add words as letter nodes to the Trie', () => {
       trie.insert('pizza');
 
-      expect(trie.root.left.data).to.equal('p');
-      expect(trie.root.left.left.data).to.equal('i');
-    });
-
-    it('should keep count of words in the Trie', () => {
-      trie.insert('pizza');
-
+      expect(trie.root.children.p.value).to.equal('p');
+      expect(trie.root.children.p.children.i.value).to.equal('i');
       expect(trie.count).to.equal(1);
     });
 
-    it('should flag the node at the end of a complete word', () => {
-      trie.insert('pizza');
-      expect(trie.root.left.left.completeWord).to.equal(false);
-      expect(trie.root.left.left.left.completeWord).to.equal(false);
-      expect(trie.root.left.left.left.left.left.completeWord).to.equal(true);
-    });
-
-    it('should be able to add other words as letter nodes to the Trie', () => {
+    it('should be able to add two words', () => {
       trie.insert('pizza');
       trie.insert('apple');
+      let keys = Object.keys(trie.root.children)
+
+      expect(keys).to.deep.equal(['p', 'a']);
+    });
+
+    it('should be able to add words that extend beyond an already inserted word', () => {
+      trie.insert('pal');
+      trie.insert('pan')
+      trie.insert('palomino')
+      trie.insert('pram')
+      let keys = Object.keys(trie.root.children.p.children.a.children)
+
+// console.log(JSON.stringify(trie, null, 4));
+      expect(keys).to.deep.equal(['l', 'n'])
+      expect(trie.count).to.equal(4);
+    });
+
+    it('should not increase count if word is a duplicate', () => {
+      trie.insert('apple');
+      trie.insert('apple');
+
+      expect(trie.count).to.equal(1);
+      expect(trie.insert('apple')).to.equal('Duplicate word')
+    });
+
+    it('should not care about capitalization', () => {
+      trie.insert('Pizza');
+      trie.insert('APPLE');
 
       expect(trie.count).to.equal(2);
     });
 
-    it.skip('should be able to add on to an existing word to make a longer word', () => {
-      trie.insert('pizza');
-      trie.insert('apple');
-      trie.insert('pizzeria')
-// console.log(JSON.stringify(trie, null, 4))
+  });//end describe INSERT
 
-      expect(trie.count).to.equal(3);
-      expect(trie.root.left.left.left.left.right.data).to.equal('e');
-    }); 
-  });
 });
